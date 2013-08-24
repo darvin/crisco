c1f84829f50dcb4250c33fefdf08e9312f25afe6
Sergey Klimov <sergey.v.klimov@gmail.com>
2013-08-24 13:42:14 -0700

    travis & testing

--- test/test.js
+++ test/test.js
@@ -1,3 +1,15 @@
-var request = require("request");
+describe('Crisco server', function(){
+  describe('review requests', function(){
+    it('should be sent when user puts #reviewthis in commit message');
+    it('should be sent when user puts #reviewthis in commit comment (on Github) and then webhook triggered');
+    it("should be sent to repo's watchers");
+    it('should be sent to person @mentioned at review request');
+    it('should be sent to persons who watch #tag');
+  });
+  describe('review comments', function(){
+    it('should be sent when user replies on commit comment (on Github) and then webhook triggered');
+    it('should be sent when user replies on review request email');
 
 
+  })
+})
